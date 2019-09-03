a201963932024ed76faffcddc675f159fad197e0
Anderson Santos Ferreira <havour@gmail.com>
2019-09-03 19:38:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/anderson/fakezap/activity/ValidadorActivity.java
@@ -0,0 +1,14 @@
+package com.example.anderson.fakezap.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import com.example.anderson.fakezap.R;
+import android.os.Bundle;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
